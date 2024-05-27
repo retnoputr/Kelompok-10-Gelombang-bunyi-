0d0bbed2c63a29baf97d44c10d33fab19fdc0bde
nandadotexe <dmasmiv@gmail.com>
2024-05-27 07:21:21 +0700

    09.11.3207 Miftakhurrokhmat

---
+++ introduction/AnandaMiftakhurrokhmat.java
@@ -0,0 +1,16 @@
+
+public class AnandaMiftakhurrokhmat {
+
+    public static void main(String[] args) {
+        String fullname = "Ananda Miftakhurrokhmat";
+        String nim = "09.11.3207";
+        String team = "Single Fighter";
+        String role = "Leader";
+
+        System.out.println("Halo, perkenalkan nama saya " + fullname + ".");
+        System.out.println("Nim saya = " + nim + ".");
+        System.out.println("Saya tergabung dalam kelompok " + team + ".");
+        System.out.println("Peran saya disini sebagai " + role + ".");
+    }
+
+}
